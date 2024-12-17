33e6059c9eb582302153fd9ddcf776d9dbdd34de
martafrancomtz <martafrancomtz@alumnos.cei.es>
2024-12-17 15:26:18 +0100

    jojo

--- semana 8/día 37 (16-12-24)/ejercicio3.js
+++ semana 8/día 37 (16-12-24)/ejercicio3.js
@@ -1,26 +1,32 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let currentIndex = 0;
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const itemList = document.getElementById("item-list");
+const items = document.querySelectorAll(".item");
+
+const totalItems = items.length;
+
+function moveToSlide(index) {
+    const angle = -index * 100;
+    itemList.style.transform = `translateX(${angle}%)`; 
+    currentIndex = index;
 }
 
-function currentSlides(n) {
-    showSlides(slideIndex = n);
+function autoMove() {
+    currentIndex = (currentIndex + 1) % totalItems; 
+    moveToSlide(currentIndex);
 }
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("my-slides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className +=" active";
-}
+
+setInterval(autoMove, 3000);
+
+prev.addEventListener("click", function () {
+    currentIndex = (currentIndex === 0 ? totalItems - 1 : currentIndex - 1);
+    moveToSlide(currentIndex);
+});
+
+next.addEventListener("click", function () {
+    currentIndex = (currentIndex === totalItems - 1 ? 0 : currentIndex + 1); 
+    moveToSlide(currentIndex);
+});
